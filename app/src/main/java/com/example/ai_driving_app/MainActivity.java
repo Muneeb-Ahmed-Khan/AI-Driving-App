687b774369627f7d8408afe7d5d815890341df52
Muneeb Ahmed <49415395+Muneeb-Ahmed-Khan@users.noreply.github.com>
2023-10-08 22:40:40 +0500

    Removed error "Can not extract resource from com.android.aaptcompiler.ParsedResource@-----"
    Code refactoring.

--- app/src/main/java/com/example/ai_driving_app/MainActivity.java
+++ app/src/main/java/com/example/ai_driving_app/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ai_driving_app;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,7 +12,17 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.content_main);
+
+        setContentView(R.layout.activity_main);
+
+        Button quizButton = findViewById(R.id.quizButton);
+        quizButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Quiz.class);
+                startActivity(intent);
+            }
+        });
 
     }
 
